bca5e70bf0f7f85fd662fdbb22d3004623a82dec
HectRam <green_cloud_@hotmail.com>
2016-10-12 03:51:52 -0500

    Files

---
+++ Practica3/tronador02.asm
@@ -0,0 +1,28 @@
+e1	DeCa	$0
+	LEYA	#15
+		INCA
+E2	LDAA	%11
+	JMP	,PC
+E3	CLRA
+	cod
+	MAX	[100,X]
+	ANDA	[20]
+	BRSET	@15
+e4		LDAA	12
+ CLR 10
+ BLO 1000
+ LBLO 0
+ cLc
+ lbgt 1
+m1 mul
+ mina
+  tfr  usa
+ASLD 123
+ ASLD a
+ ASRB
+  		trap		
+ xgd x
+ exg
+  		  eTbL x
+    tfr
+    TbnE
