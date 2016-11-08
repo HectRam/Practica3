315dce66bc9fe8a41df913cf9b7587e6e72b5f03
HectRam <green_cloud_@hotmail.com>
2016-11-08 09:54:13 -0600

    Final

---
+++ Practica3/comentarios.asm
@@ -0,0 +1,13 @@
+
+KSHAKSJHDSAJKDKDJSALDKSAJDLKSAJDLKASJDLSA 
+A = 100 
+X = 1234 
+Hexadecimal Constants 
+A = $64 = 100 
+A = $9c = -100 
+X = $1234 = 4660 
+Binary Constants 
+A = $64 = 100 
+Most significant 
+nibble mask 
+X = $1234 
